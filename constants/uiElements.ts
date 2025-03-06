2688d6ee7c4004de7a5fc1e5296da60d51e1d829
Polina Tcaciuc <polinatcaciuc@gmail.com>
2025-03-06 16:36:04 +0300

    fix: Пополнить объект UI компонентов

--- constants/uiElements.ts
+++ constants/uiElements.ts
@@ -1,9 +1,17 @@
-import { ElRadioGroup, ElRadio, ElSelect, ElOption,ElInputNumber } from "element-plus";
+import {
+  ElRadioGroup,
+  ElRadio,
+  ElSelect,
+  ElOption,
+  ElInputNumber,
+  ElInput,
+} from "element-plus";
 
-export const UI_ELEMENTS: {[key: string]: any} = {
+export const UI_ELEMENTS: { [key: string]: any } = {
   "el-radio-group": ElRadioGroup,
   "el-radio": ElRadio,
   "el-select": ElSelect,
   "el-option": ElOption,
-  "el-input-number":ElInputNumber
+  "el-input-number": ElInputNumber,
+  "el-input": ElInput,
 };
