8923e6a112ba93f0bbc335d6cb1d5f7c05f06acd
Polina Tcaciuc <polinatcaciuc@gmail.com>
2025-03-06 16:56:40 +0300

    fix: Поправить шапку сайта

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -4,18 +4,21 @@
   <header class="header">
     <div class="container">
       <div class="header__inner">
-        <BaseLogo />
+       <h1 class="title_large">Calculadora de ROI para proyectos de IoT + AI</h1> 
       </div>
     </div>
   </header>
 </template>
-<style lang="scss" scoped>
+<style lang="scss">
 .header {
   padding: 10px 0;
   &__inner {
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+  h1{
+    text-align: center
   }
 }
 </style>
